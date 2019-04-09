d9e657f67b7f6373ef0acb4b991eba1c6521dc3e
eastandrew <38232641+eastandrew@users.noreply.github.com>
2019-04-08 21:16:55 -0400

    plot size

--- app.R
+++ app.R
@@ -29,7 +29,7 @@ ui <- fluidPage(
     
     # Show a plot of the generated concentrations across bio effect of 100% across number of treatments
     mainPanel(
-      plotOutput("scatterPlot")
+      plotOutput("scatterPlot", width=500, height=400)
     )
   )
 )
@@ -47,6 +47,7 @@ server <- function(input, output) {
       vect[i] <- vect[i-1]*multiplier
     }
     #vect
+    par(mai=c(1,1,0.1,0.1))
     plot(vect,c(seq(1,0,length.out=input$numT)), log="x", type="b", pch=NA, xlab="log visualized concentration", ylab="effect")
     text(vect,c(seq(1,0,length.out=input$numT)),round(vect,3))
     text(vect[(length(vect)-1)], 0.8, bquote(paste("Factor: ",.(round(multiplier,3)))))
@@ -62,6 +63,7 @@ server <- function(input, output) {
       df$ID <- as.numeric(row.names(df))-1
       df$treat <- df$treatstart*(df$factor^df$ID)
       #df
+      par(mai=c(1,1,0.1,0.1))
       plot(1,1,pch=NA, ylim=c(0,1),xlim=c(min(df$treat),max(df$treat)),xlab="log visualized concentration",ylab="effect",log="x")
       points(effmag~treat, data=df, pch=NA, type="b")
       text(effmag~treat, data=df, bquote(.(round(df$treat,3))))
